fd0b394006941594329e4dc41157041a5f543e18
dave long <davelongdev@gmail.com>
2024-08-12 14:19:12 -0400

    write normalizeURL function and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,44 @@
+// Import jest functions
 import { test, expect } from "@jest/globals";
+
+// Impoprt normalizeURL function from crawl.js
 import { normalizeURL } from "./crawl.js";
 
-const crawl = require('./crawl');
+test('normalizeURL strip protocol', () => {
 
-test('takes ')
+  // should return the URL with no protocol
+  const input = 'https://test.com/path'
+  const actual = normalizeURL(input)
+  const expected = 'test.com/path'
+  expect(actual).toEqual(expected)
+})
+
+test('normalizeURL strip trailing slash', () => {
+  
+  // should return the URL with no trailing slash
+  const input = 'https://test.com/path/'
+  const actual = normalizeURL(input)
+  const expected = 'test.com/path'
+  expect(actual).toEqual(expected)
+
+})
+
+test('normalizeURL make case lower', () => {
+  
+  // should return the URL with no trailing slash
+  const input = 'https://TEST.com/path/'
+  const actual = normalizeURL(input)
+  const expected = 'test.com/path'
+  expect(actual).toEqual(expected)
+
+})
+
+test('normalizeURL strip http', () => {
+  
+  // should return the URL with no trailing slash
+  const input = 'http://test.com/path/'
+  const actual = normalizeURL(input)
+  const expected = 'test.com/path'
+  expect(actual).toEqual(expected)
+
+})
