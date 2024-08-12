7cfa2df377a75309c459b52150fc78bfbcd576bf
dave long <davelongdev@gmail.com>
2024-08-12 16:03:58 -0400

    getURLsFromHTML function and tests

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+import { JSDOM } from 'jsdom'
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const linkElements = dom.window.document.querySelectorAll('a')
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === '/') {
+      // relative URLs
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`)
+        urls.push(urlObj.href)
+      } catch (err){
+        console.log(`error with relative url: ${err.message}`)
+      }
+    } else {
+      // absoulute URLs
+      try {
+        const urlObj = new URL(linkElement.href)
+        urls.push(urlObj.href)
+      } catch (err){
+        console.log(`error with absolute url: ${err.message}`)
+      }
+    }
+  }
+  return urls
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString)
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`
@@ -8,3 +36,4 @@ function normalizeURL(urlString) {
 }
 
 export { normalizeURL };
+export { getURLsFromHTML };
--- crawl.test.js
+++ crawl.test.js
@@ -2,7 +2,78 @@
 import { test, expect } from "@jest/globals";
 
 // Impoprt normalizeURL function from crawl.js
-import { normalizeURL } from "./crawl.js";
+import { getURLsFromHTML, normalizeURL } from "./crawl.js";
+
+test('getURLsFromHTML absolute', () => {
+
+  // should return the URL with no protocol
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="https://test.com/path"><span>Go to test.com</span></a>
+    </body>
+</html>
+`
+  const inputBaseURL = 'https://test.com'
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ['https://test.com/path']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative', () => {
+
+  // should return the URL with no protocol
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path/"><span>Go to test.com</span></a>
+    </body>
+</html>
+`
+  const inputBaseURL = 'https://test.com'
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ['https://test.com/path/']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML relative and absolute', () => {
+
+  // should return the URL with no protocol
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="/path1/">
+          Go to test.com/path1
+        </a>
+        <a href="https://test.com/path2/">
+          Go to test.com/path2
+        </a>
+    </body>
+</html>
+`
+  const inputBaseURL = 'https://test.com'
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = ['https://test.com/path1/', 'https://test.com/path2/']
+  expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML invalid URL', () => {
+
+  // should return the URL with no protocol
+  const inputHTMLBody = `
+<html>
+    <body>
+        <a href="invalid">
+          Go to invalid URL
+        </a>
+    </body>
+</html>
+`
+  const inputBaseURL = 'https://test.com'
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+  const expected = []
+  expect(actual).toEqual(expected)
+})
 
 test('normalizeURL strip protocol', () => {
 
