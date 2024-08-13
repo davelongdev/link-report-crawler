b22082b0496e9c38ea58153dcb9dd45b9b1ff494
dave long <davelongdev@gmail.com>
2024-08-12 22:02:52 -0400

    crawl page recursive working

--- crawl.js
+++ crawl.js
@@ -1,27 +1,61 @@
 import { JSDOM } from 'jsdom'
 
-async function crawlPage(currentURL) {
+// base url - web site domain
+// current url - page actively being crawled
+// pages - object where K is normalized url and V is number of links
+
+async function crawlPage(baseURL, currentURL, pages) {
+
+  const baseURLobj = new URL(baseURL)
+  const currentURLobj = new URL(currentURL)
+
+  // check if a url is on the original domain (basecase 1)
+  if(baseURLobj.hostname !== currentURLobj.hostname) {
+    return pages
+  }
+  
+  // check if already crawled the current page (basecase 2)
+  const normalizedcurrentURL = normalizeURL(currentURL)
+  if (pages[normalizedcurrentURL] > 0) {
+    pages[normalizedcurrentURL]++
+    return pages
+  }
+  
+  pages[normalizedcurrentURL] = 1
+
   console.log(`actively crawling ${currentURL}`)
 
   try {
-  const resp = await fetch(currentURL)
+    const resp = await fetch(currentURL)
 
-  // check if status code under 400
-  if (resp.status > 399) {
-      console.log(`error in fetch with status code ${resp.status} on page ${currentURL}`)
-      return
+    // check if status code under 400
+    if (resp.status > 399) {
+        console.log(`error in fetch with status code ${resp.status} on page ${currentURL}`)
+        return pages
+      }
+
+    // check if getting html
+    const contentType = resp.headers.get('content-type')
+    if (!contentType.includes('text/html')) {
+        console.log(`non html response. content type ${contentType} on page ${currentURL}`)
+      return pages
     }
-  // check if getting html
-  const contentType = resp.headers.get('content-type')
-  if (!contentType.includes('text/html')) {
-      console.log(`non html response. content type ${contentType} on page ${currentURL}`)
+    
+    // store html in variable
+    const htmlBody = await resp.text()
+   
+    // call function to extract URLS from htmlBody
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL)
+    
+    // recursively call crawlPage until above base cases are met
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages)
     }
-  // log out the html
-  console.log(await resp.text())
 
   } catch (err) {
     console.log(`error in fetch. ${err.message} on page ${currentURL}`)
   }
+  return pages
 }
 
 function getURLsFromHTML(htmlBody, baseURL) {
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { crawlPage } from './crawl.js'
 
-function main() {
+async function main() {
   if (process.argv.length < 3) {
     console.log('no website provided')
     process.exit(1)
@@ -9,13 +9,16 @@ function main() {
     console.log('too many command line agrs')
     process.exit(1)
   }
-  for (const arg of process.argv) {
-    console.log(arg)
-  }
+
   const baseURL = process.argv[2]
 
   console.log(`starting crawl of ${baseURL}`)
-  crawlPage(baseURL)
+
+  const pages = await crawlPage(baseURL, baseURL, {})
+
+  for (const page of Object.entries(pages)) {
+    console.log(page)
+  }
 }
 
 main()
